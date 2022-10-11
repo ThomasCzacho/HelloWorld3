a85aa5963c6eca01f6be72af27f3dd257b648407
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-10-11 16:35:55 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("a message other than “Hello World!”.");
+
+    }
+}
+ //java is case sensitive
+//semicolons are important to java
